1d7eb002442f81e37952f58f6eef2348ad011ecf
steven.yeo <steven.y@dynrtech.com>
2023-09-04 22:04:41 +0700

    20230904 adding cart context and cart icon & dropdown

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
+
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,7 +3,11 @@ import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { useContext } from "react";
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.util";
 
@@ -11,7 +15,7 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
-    console.log('currentUser: ' + currentUser);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -36,7 +40,13 @@ const Navigation = () => {
                             </Link>
                         )
                     }
+
+                    <CartIcon />
                 </div>
+
+                {
+                    isCartOpen && <CartDropdown />
+                }
             </div>
 
             <Outlet />
